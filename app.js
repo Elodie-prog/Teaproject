b3dddf0ded187ce87c5d0b61e114e3761a4e765d
Elodie-prog <elodiepacheco@yahoo.fr>
2021-05-29 11:26:24 +0200

    config env created

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config({path: './config/config.env'});
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT;
 
 const notes = require('./routes/notes');
+const users = require('./routes/user');
 
+app.use('/user', users);
 app.use('/notes', notes);
 
 
-app.listen (PORT, () =>{})
-    
+app.listen (PORT, () =>{
+    console.log ( `Express started on http://localhost:${PORT} in ${process.env.NODE_ENV} mode`);
+})
+
+
